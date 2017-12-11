0e74d3a054ee1bc16cd016ff7b4e5ee91a7c7adb
R.miyagi <R.miyagi>
2017-12-11 09:09:33 +0900

    ベースとなるプロジェクトをコミット

---
+++ PureServlet/src/servlet/LoginServlet.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author r_miyagi
+ */
+@WebServlet("/Login")
+@SuppressWarnings("serial")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doProcess(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doProcess(req, res);
+	}
+
+	protected void doProcess(HttpServletRequest req, HttpServletResponse res) {
+
+	}
+}
